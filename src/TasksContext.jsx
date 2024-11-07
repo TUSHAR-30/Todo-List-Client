063fad397f86f75b142dba55c34d5d59ebd808e3
Tushar <tusharchawla2002@gmail.com>
2024-11-08 01:27:25 +0530

    Added No Tasks are present animation

--- src/TasksContext.jsx
+++ src/TasksContext.jsx
@@ -9,8 +9,8 @@ export function TasksProvider({ children }) {
     const [tasks, setTasks] = useState([
         { id: 1, title: 'Jogging', isCompleted: false , description:"This is Jogging" ,dueDate:"2024-11-28" },
         { id: 2, title: 'Cricket Practice', isCompleted: false , description:"This is Cricket" ,dueDate:"2026-02-12" },
-        { id: 3, title: 'Completing DSA course', isCompleted: false , description:"This is DSA" ,dueDate:"2024-08-02"},
-        { id: 4, title: 'Yoga', isCompleted: false , description:"This is Yoga" ,dueDate:"2024-02-20"},
+        { id: 3, title: 'Completing DSA course', isCompleted: true , description:"This is DSA" ,dueDate:"2024-12-02"},
+        { id: 4, title: 'Yoga', isCompleted: false , description:"This is Yoga" ,dueDate:"2024-11-20"},
     ]);
 
     return (
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Task from './Task'
 
 import "./Tasks.css"
+import TasksContext from '../../TasksContext'
 function Tasks() {
-    
+    const { tasks, setTasks } =useContext(TasksContext)
+
     return (
         <div className='tasks'>
-           <Task />
-        </div>
+            {tasks.length!=0?(
+            <Task />
+            ):(
+                <>
+                 <div className='notasks-animation' >
+                <dotlottie-player
+                    src="animation.json"
+                    speed="1"
+                    autoplay
+                ></dotlottie-player>
+                </div>
+              <h3 style={{textAlign:'center'}}>No Tasks are present</h3>
+                </>
+           
+            )
+        }
+           </div>
     )
 }
 
