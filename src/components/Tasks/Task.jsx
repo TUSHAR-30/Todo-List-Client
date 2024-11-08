15d428e1c13d4fd0bcaba248bc0c26513e9dfe82
Tushar <tusharchawla2002@gmail.com>
2024-11-08 13:19:29 +0530

    Implemented Dragging of tasks functionality for pointer devices

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,19 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import TasksContext from '../../TasksContext';
-import { MdDeleteOutline } from "react-icons/md";
-import { GrEdit } from "react-icons/gr";
-import "./Task.css"
+import { MdDeleteOutline } from 'react-icons/md';
+import { GrEdit } from 'react-icons/gr';
+import './Task.css';
 import Modal from '../../assets/Modal/ModalOverlay/Modal';
 import EditTaskModalContent from '../../assets/Modal/EditTaskModalContent/EditTaskModalContent';
 import ViewTaskModalContent from '../../assets/Modal/ViewTaskModalContent/ViewTaskModalContent';
-function Task() {
-    const { tasks, setTasks } = useContext(TasksContext);
+
+function Task({ task, index, onDragStart, onDragOver, onDrop }) {
+    const { setTasks } = useContext(TasksContext);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [openedTask, setOpenedTask] = useState({});
-    const [openedTaskOperation, setOpenedTaskOperation] = useState("")
+    const [openedTaskOperation, setOpenedTaskOperation] = useState('');
 
     function deleteTask(e, taskId) {
-        e.stopPropagation()
+        e.stopPropagation();
         setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
     }
 
@@ -26,43 +27,58 @@ function Task() {
     }
 
     function openModal(e, task, operation) {
-        e.stopPropagation()
-        setOpenedTask(task)
-        setIsModalOpen(true)
-        setOpenedTaskOperation(operation)
+        e.stopPropagation();
+        setOpenedTask(task);
+        setIsModalOpen(true);
+        setOpenedTaskOperation(operation);
     }
 
     return (
         <>
-            {tasks.map((task) => {
-                return (
-                    <div key={task.id} className='task' onClick={(e) => { openModal(e, task, "view") }}>
-                        <div onClick={(e) => e.stopPropagation()}>
-                            <input type='checkbox' checked={task.isCompleted} onChange={() => toggleTaskCompletion(task.id)} />
-                        </div>
-                        <p className={`task-title ${task.isCompleted ? "completed-task" : ""}`}>{task.title}</p>
-                        <div className='task-deleteicon'>
-                            <MdDeleteOutline size={20} onClick={(e) => deleteTask(e, task.id)} />
-                        </div>
-                        <div className='task-editicon' onClick={(e) => openModal(e, task, "edit")}>
-                            <GrEdit size={17} />
-                        </div>
-                    </div>
-                )
-            })}
+            <div
+                className='task'
+                draggable
+                onDragStart={() => onDragStart(index)}
+                onDragOver={onDragOver}
+                onDrop={() => onDrop(index)}
+                onClick={(e) => openModal(e, task, 'view')}
+            >
+                <div onClick={(e) => e.stopPropagation()}>
+                    <input
+                        type='checkbox'
+                        checked={task.isCompleted}
+                        onChange={() => toggleTaskCompletion(task.id)}
+                    />
+                </div>
+                <p className={`task-title ${task.isCompleted ? 'completed-task' : ''}`}>
+                    {task.title}
+                </p>
+                <div className='task-deleteicon'>
+                    <MdDeleteOutline size={20} onClick={(e) => deleteTask(e, task.id)} />
+                </div>
+                <div
+                    className='task-editicon'
+                    onClick={(e) => openModal(e, task, 'edit')}
+                >
+                    <GrEdit size={17} />
+                </div>
+            </div>
             <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                {
-                    openedTaskOperation == "edit" ? (
-                        <EditTaskModalContent openedTask={openedTask} setOpenedTask={setOpenedTask} setIsModalOpen={setIsModalOpen} />
-                    ) :
-                        (
-                            <ViewTaskModalContent openedTask={openedTask} setIsModalOpen={setIsModalOpen} />
-                        )
-                }
-
+                {openedTaskOperation === 'edit' ? (
+                    <EditTaskModalContent
+                        openedTask={openedTask}
+                        setOpenedTask={setOpenedTask}
+                        setIsModalOpen={setIsModalOpen}
+                    />
+                ) : (
+                    <ViewTaskModalContent
+                        openedTask={openedTask}
+                        setIsModalOpen={setIsModalOpen}
+                    />
+                )}
             </Modal>
         </>
-    )
+    );
 }
 
-export default Task
+export default Task;
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,31 +1,47 @@
-import React, { useContext } from 'react'
-import Task from './Task'
+import React, { useContext, useState } from 'react';
+import Task from './Task';
+import './Tasks.css';
+import TasksContext from '../../TasksContext';
 
-import "./Tasks.css"
-import TasksContext from '../../TasksContext'
 function Tasks() {
-    const { tasks, setTasks } =useContext(TasksContext)
+    const { tasks, setTasks } = useContext(TasksContext);
+    const [draggedTaskIndex, setDraggedTaskIndex] = useState(null);
+
+    // Handler to manage the drop and reorder tasks
+    const handleDrop = (index) => {
+        if (draggedTaskIndex === null) return;
+
+        const updatedTasks = [...tasks];
+        const [movedTask] = updatedTasks.splice(draggedTaskIndex, 1);
+        updatedTasks.splice(index, 0, movedTask);
+
+        setTasks(updatedTasks);
+        setDraggedTaskIndex(null); // Reset the dragged task index after drop
+    };
 
     return (
         <div className='tasks'>
-            {tasks.length!=0?(
-            <Task />
-            ):(
+            {tasks.length !== 0 ? (
+                tasks.map((task, index) => (
+                    <Task
+                        key={task.id}
+                        task={task}
+                        index={index}
+                        onDragStart={() => setDraggedTaskIndex(index)}
+                        onDragOver={(e) => e.preventDefault()} // Prevent default to allow drop
+                        onDrop={() => handleDrop(index)}
+                    />
+                ))
+            ) : (
                 <>
-                 <div className='notasks-animation' >
-                <dotlottie-player
-                    src="animation.json"
-                    speed="1"
-                    autoplay
-                ></dotlottie-player>
-                </div>
-              <h3 style={{textAlign:'center'}}>No Tasks are present</h3>
+                    <div className='notasks-animation'>
+                        <dotlottie-player src="animation.json" speed="1" autoplay></dotlottie-player>
+                    </div>
+                    <h3 style={{ textAlign: 'center' }}>No Tasks are present</h3>
                 </>
-           
-            )
-        }
-           </div>
-    )
+            )}
+        </div>
+    );
 }
 
-export default Tasks
+export default Tasks;
